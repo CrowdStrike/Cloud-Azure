f4c14b6164509222beb66849c3420b5cfb29e4eb
Gax <gax.theodorio@crowdstrike.com>
2024-06-03 11:05:36 -0400

    add shebang

--- storage-account-container-protection/bin/get-findings.sh
+++ storage-account-container-protection/bin/get-findings.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 az monitor app-insights query --app APP_INSIGHTS_APP_ID --analytics-query "traces | where message has \"Threat\" or message has \"Mitigate\" | project message" --output jsonc
