8cfda75c38219e76611483228b544508a0034b37
Gabe Alford <redhatrises@gmail.com>
2024-11-08 08:42:35 -0700

    chore: remove old content that is no longer supported

--- container/falcon-container-terraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "resource-group" {
-  name     = "${var.resource_group_name}${var.prefix}${var.cloud_region}${var.suffix}"
-  location = var.cloud_region
-  tags = var.tags
-}
-
-data "azurerm_client_config" "current" {
-}
